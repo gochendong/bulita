217e3590ba2a1cecd0549de1a88c04a39b51df14
chendong <chendong@chipsecwethings.com>
2026-02-02 18:11:39 +0800

    fix

--- packages/web/src/modules/Sidebar/About.tsx
+++ packages/web/src/modules/Sidebar/About.tsx
@@ -42,11 +42,6 @@ function About(props: AboutProps) {
                     </a>
                 </div>
                 <div className={Common.block2}>
-                    <p className={Common.title}>
-                        打包方式: {process.env.BUNDLER || 'webpack'}
-                    </p>
-                </div>
-                <div className={Common.block2}>
                     <p>
                         Powered by
                         <a
